fd76e3acad4bcaf93d3b781f969f1421eed05400
minjun0423 <162403690+minjun0423@users.noreply.github.com>
2024-05-10 16:37:09 +0900

    0510_1637

--- app/main.py
+++ app/main.py
@@ -9,7 +9,7 @@ app =FastAPI()
 #    return {"message": "Hello Root!"}
 
 def read_root():
-    URL = "http://swopenAPI.seoul.go.kr/api/subway/427a53796b6d696e37344f6451594a/xml/realtimeStationArrival/0/5/¼­¿ï"
+    URL = "https://bigdata.kepco.co.kr/openapi/v1/powerUsage/industryType.do?year=2020&month=11&metroCd=11&cityCd=110&bizCd=C&apiKey=69viVTSCMlJ552B9O243czcn718eth0C5691acbA&returnType=json"
 
     contents = requests.get(URL).text
 
