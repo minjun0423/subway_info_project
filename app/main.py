d8e615ad4e8dad184217c0062b5e45d8206f5a83
minjun0423 <162403690+minjun0423@users.noreply.github.com>
2024-05-16 18:24:11 +0900

    test

--- app/main.py
+++ app/main.py
@@ -3,26 +3,31 @@ from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 import requests
 
-import requests
-
 app =FastAPI()
+templates = Jinja2Templates(directory="templates")
 
-@app.get("/")
+@app.get("/", response_class=HTMLResponse)
+async def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
 
-def read_root():
-    URL = "http://swopenapi.seoul.go.kr/api/subway/427a53796b6d696e37344f6451594a/xml/realtimeStationArrival/0/5/%EC%84%9C%EC%9A%B8"
+@app.post("/search", response_class=HTMLResponse)
+async def search(request: Request, station_name: str = Form(...)):
+    return templates.TemplateResponse("result.html", {"request": request, "station_name": station_name})
+
+#def read_root():
+#    URL = "http://swopenapi.seoul.go.kr/api/subway/427a53796b6d696e37344f6451594a/xml/realtimeStationArrival/0/5/%EC%84%9C%EC%9A%B8"
 #지하철 실시간 도착정보 불러오기
 
 #http://swopenapi.seoul.go.kr/api/subway/427a53796b6d696e37344f6451594a/xml/realtimePosition/0/5/1%ED%98%B8%EC%84%A0
 #지하철 실시간 열차 위치정보
 
-    contents = requests.get(URL).text
+#    contents = requests.get(URL).text
 
-    return {"message": contents}
+#    return {"message": contents}
 
-@app.get("/home")
-def home():
-    return {"message": "home!"}
+#@app.get("/home")
+#def home():
+#    return {"message": "home!"}
 
 
 @app.get("/subway/{station_name}")
