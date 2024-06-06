a6cad9e1b59fe6214641b3b7e9677ac3499e699d
minjun0423 <162403690+minjun0423@users.noreply.github.com>
2024-06-06 22:34:02 +0900

    test

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 templates = Jinja2Templates(directory="app/templates")
 app.mount("/static", StaticFiles(directory="app/static"), name="static")
 
-@app.get("/")
+@app.get("/", response_class=HTMLResponse)
 def read_root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
