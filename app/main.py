4055b88b339c00d70ba23f721d3ffb73be088331
minjun0423 <162403690+minjun0423@users.noreply.github.com>
2024-06-06 22:40:35 +0900

    test

--- app/main.py
+++ app/main.py
@@ -1,14 +1,12 @@
 from fastapi import FastAPI, HTTPException, Request
 from fastapi.responses import HTMLResponse
-from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 import requests
 
 app = FastAPI()
 templates = Jinja2Templates(directory="app/templates")
-app.mount("/static", StaticFiles(directory="app/static"), name="static")
 
-@app.get("/", response_class=HTMLResponse)
+@app.get("/")
 def read_root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
@@ -16,6 +14,7 @@ def read_root(request: Request):
 def subway_info():
     return {"지하철 테스트"}
 
-@app.get("/route_map", response_class=HTMLResponse)
-def route_map(request: Request):
-    return templates.TemplateResponse("route_map.html", {"request": request})
+@app.get("/route_map")
+def subway_info():
+    return {"맵"}
+
